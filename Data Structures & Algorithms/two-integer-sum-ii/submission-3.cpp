e9f1d8bc02601afaf263d584294ce916032be435
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2026-06-14 13:17:16 +0530

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int tar) {
+        for(int i=0;i<nums.size();++i){
+            int low = i+1,high = nums.size()-1;
+            while(low<=high){
+                int mid = low + (high-low)/2;
+                if (nums[mid]+nums[i]==tar) return {i+1,mid+1};
+                else if (nums[mid] + nums[i] > tar) high = mid-1;
+                else if (nums[mid] + nums[i]< tar) low = mid+1;
+                cout<< i<<mid<< low<<high<<endl;
+            }
+        }
+        return {};
+    }
+};

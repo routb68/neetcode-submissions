1050cda00cbab3777acb102269a5b79f18a4e143
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2026-06-15 10:15:26 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        vector<int>ans(temp.size());
+        for(int i=temp.size()-2;i>=0;--i){
+            int j = i+1;
+            while(j<temp.size() and temp[j]<=temp[i]){
+                if(ans[j]==0){
+                    j=INT_MAX;break;
+                }else{
+                    j+=ans[j];
+                }
+            }
+            if(j<temp.size()){
+                ans[i] = j-i;
+            }
+        }
+        return ans;
+    }
+};

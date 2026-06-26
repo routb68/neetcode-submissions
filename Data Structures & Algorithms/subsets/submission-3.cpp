85a1c53c64cd3a27b29430a19950fdde07bee063
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2026-06-26 09:48:13 +0530

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>temp;
+        vector<vector<int>>ans;
+        for(int i=0;i< (1<<nums.size());++i){
+            for(int j=0;j<nums.size();++j){
+                if(i & (1<<j))temp.push_back(nums[j]);
+            }
+            ans.emplace_back(temp);
+            temp.clear();
+        }
+        return ans;
+    }
+};

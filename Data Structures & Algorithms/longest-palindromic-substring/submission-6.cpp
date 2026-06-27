b4dd6a577f96c422c946661b9794123410a6192c
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2026-06-27 22:06:42 +0530

    Add: longest-palindromic-substring - submission-6

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-6.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int max_len=0; string ans ="";
+        vector<vector<bool>>dp(s.size(),vector<bool>(s.size(),false));
+        for(int i=s.size()-1;i>=0;--i){
+            for(int j = i;j<s.size();++j){
+                if(s[i]==s[j] and (j-i<=2 or dp[i+1][j-1])){
+                    dp[i][j]=true;
+                    if(max_len<j-i+1){
+                        max_len = j-i+1;
+                        ans = s.substr(i,j-i+1);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};

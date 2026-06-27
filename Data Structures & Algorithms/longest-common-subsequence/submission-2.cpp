f3c9eb469406beb4b6671289286dc6010bf61ece
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2026-06-28 02:33:31 +0530

    Add: longest-common-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string s1, string s2) {
+        vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,0));
+        for(int i=s1.size()-1;i>=0;--i){
+            for(int j=s2.size()-1;j>=0;--j){
+                if(s1[i]==s2[j]) dp[i][j] = 1+dp[i+1][j+1];
+                else dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
+    }
+};

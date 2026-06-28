eff8afa7a6e821fafda2222f6a4e5ee6341b2d2d
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2026-06-29 01:48:07 +0530

    Add: gas-station - submission-0

---
+++ Data Structures & Algorithms/gas-station/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        if(accumulate(gas.begin(),gas.end(),0)< accumulate(cost.begin(),cost.end(),0)) return -1;
+        int tank=0;
+        int pidx =0;
+        for(int i=0;i<gas.size();++i){
+            tank += (gas[i]-cost[i]);
+            if(tank<0){
+                tank=0;
+                pidx=i+1;
+            }
+        }
+        return pidx;
+    }
+};

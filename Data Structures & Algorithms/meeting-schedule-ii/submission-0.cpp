2a6634532dd0eca222821a932f8be6630f869924
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2026-06-28 23:52:07 +0530

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        sort(intervals.begin(),intervals.end(),[](const Interval&a,const Interval &b){return a.start<b.start;});
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(auto &x:intervals){
+            if(pq.size() and pq.top()<=x.start){
+                pq.pop();
+            }
+            pq.push(x.end);
+        }
+        return pq.size();
+    }
+};

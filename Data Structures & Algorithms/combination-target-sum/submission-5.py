8ae5b1d1589048234f6485500860c60788a97cce
Biswajit Rout <101084372+routb68@users.noreply.github.com>
2026-09-17 10:37:18 +0530

    Add: combination-target-sum - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum/submission-5.py
@@ -0,0 +1,20 @@
+import copy
+class Solution:
+    def generate(self,nums,idx, target, temp_nums):
+        if target==0:
+            self.ans.append(copy.deepcopy(temp_nums))
+            return 
+        if target<0 or idx== len(nums):
+            return 
+        if target-nums[idx] <0:
+            return 
+        temp_nums.append(nums[idx])
+        self.generate(nums,idx,target-nums[idx],temp_nums)
+        temp_nums.pop()
+        self.generate(nums,idx+1,target,temp_nums)
+
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        self.ans = []
+        self.generate(nums, 0, target, [])
+        return self.ans
